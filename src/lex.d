5a8cddf790289d1eb74acb4d50c6c9060db62edf
Justin Gottula <justin@jgottula.com>
2012-10-03 22:51:12 -0700

    lex: fixed spacing error

--- src/lex.d
+++ src/lex.d
@@ -17,7 +17,7 @@ import std.string;
  +/
 const string[] keywords = [
 	"auto", "break", "case", "char", "const", "continue", "default", "do",
-	"double", "else", "enum", "extern", "float", "for", "goto", "if","int",
+	"double", "else", "enum", "extern", "float", "for", "goto", "if", "int",
 	"long", "register", "return", "short", "signed","sizeof", "static",
 	"struct", "switch", "typedef", "union", "unsigned", "void", "volatile",
 	"while"
