784db32898ebee39b01dfedd1a3a2add85bddafe
Justin Gottula <justin@jgottula.com>
2012-10-05 12:59:28 -0700

    cpp: added comment re: original -> preprocessed line/col mapping

--- src/cpp.d
+++ src/cpp.d
@@ -23,6 +23,18 @@ void preProcess(in string before, out string after) {
 	string cur = before;
 	char[] buffer = new char[0];
 	
+	/* TODO: invent a way to map original source lines/cols to preprocessed
+	 * lines and cols so the lexer can annotate tokens' lines/cols correctly
+	 * 
+	 * there should be a new struct with cpp context information, which will
+	 * contain a (probably) linked list containing a series of structs which
+	 * each represent an instance in which the cpp added lines, removed lines,
+	 * added columns, removed columns, etc.
+	 * 
+	 * this way, the lexer can easily compute what the _original_ line number of
+	 * a particular bit of source code was
+	 */
+	
 	while (cur.length > 0) {
 		buffer ~= cur[0];
 		cur = cur[1..$];
