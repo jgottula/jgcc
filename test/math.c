fea4d4413e55db8064f8a3ab42cb8e4fdcd246c3
Justin Gottula <justin@jgottula.com>
2012-10-05 13:16:13 -0700

    added math test

---
+++ test/math.c
@@ -0,0 +1,26 @@
+void main() {
+	int a, b, c, d, e;
+	
+	a = 0;
+	b = 1;
+	c = 2;
+	d = 3;
+	e = 4;
+	
+	a = -a;
+	
+	a = b + c;
+	b = c - d;
+	c = d * e;
+	d = e / a;
+	e = a % b;
+	
+	a += b;
+	b -= c;
+	c *= d;
+	d /= e;
+	e %= a;
+	
+	a = (b == c);
+	b = (c != d);
+}
